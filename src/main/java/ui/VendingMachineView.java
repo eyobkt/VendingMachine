1826e8d6ce5077a898e3c2c6f054aaf4301fef87
ychen8777 <ychen8777@gmail.com>
2021-09-14 16:43:04 -0400

    Update VendingMachineView.java
    add method signatures

--- src/main/java/ui/VendingMachineView.java
+++ src/main/java/ui/VendingMachineView.java
@@ -5,10 +5,57 @@
  */
 package ui;
 
+import com.sg.vendingmachine.dto.Item;
+import java.util.List;
+
 /**
  *
  * @author ychen
  */
 public class VendingMachineView {
+    
+        private UserIO io;
+
+    public VendingMachineView(UserIO io) {
+        this.io = io;
+    }
+    
+    public int printMenuAndGetSelection(List<Item> inventory) {
+        // 1 - continue to purchase
+        // 2 - exit
+        
+        return 0;
+    }
+    
+    public int printMoneyDeposit() {
+        // ask user the amount of money to input
+        return 0;
+    }
+    
+    public int printItemSelection() {
+        // ask user for the item ID
+        return 0;
+    }
+    
+    public void printInsufficientFund() {
+        
+    }
+    
+    public void printChange(List<String> changes) {
+        // successful purchase, print change
+        // changes[0] - dollar amount
+        // changes[1] - quarter amount
+        // changes[2] - dime amount
+        // changes[3] - nickel amount
+        // changes[4] - penny amount
+    }
+    
+    public void printExitMessage() {
+        // transaction finishes, exit program
+    }
+    
+    
+    
+    
     
 }
