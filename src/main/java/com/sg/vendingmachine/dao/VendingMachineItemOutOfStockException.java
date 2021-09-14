3bc1e23531ddef0d4241d42e393d50098d395de7
hiapellet <hi@apellet.ca>
2021-09-14 20:49:26 -0300

    Implemented all dao interface methods

---
+++ src/main/java/com/sg/vendingmachine/dao/VendingMachineItemOutOfStockException.java
@@ -0,0 +1,11 @@
+package com.sg.vendingmachine.dao;
+
+public class VendingMachineItemOutOfStockException extends Exception {
+    public VendingMachineItemOutOfStockException(String message) {
+        super(message);
+    }
+    
+    public VendingMachineItemOutOfStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
