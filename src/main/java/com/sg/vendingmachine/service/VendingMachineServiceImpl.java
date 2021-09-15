24800becf2612936e7e8ddc8ba68dd9e4ca20b6a
eyobkt <eyob.tesfaye@outlook.com>
2021-09-15 10:47:08 -0400

    Modify Service

--- src/main/java/com/sg/vendingmachine/service/VendingMachineServiceImpl.java
+++ src/main/java/com/sg/vendingmachine/service/VendingMachineServiceImpl.java
@@ -5,6 +5,7 @@ import com.sg.vendingmachine.dao.VendingMachinePersistenceException;
 import com.sg.vendingmachine.dto.Change;
 import com.sg.vendingmachine.dto.Item;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 /**
@@ -13,13 +14,16 @@ import java.util.List;
  */
 public class VendingMachineServiceImpl implements VendingMachineService {
     private final VendingMachineDao vendingMachineDao;
+    private BigDecimal moneyAmount;
 
     public VendingMachineServiceImpl(VendingMachineDao vendingMachineDao) {
         this.vendingMachineDao = vendingMachineDao;
+        
+        setMoneyAmountToZero();
     }
     
     @Override
-    public List<Item> getAllItems() throws NoRemainingItemsException, 
+    public List<Item> listItems() throws NoRemainingItemsException, 
             VendingMachinePersistenceException {
         
         List<Item> allItems = vendingMachineDao.getAllItems();
@@ -32,17 +36,28 @@ public class VendingMachineServiceImpl implements VendingMachineService {
         }
     }
     
+    public void putMoney(BigDecimal moneyAmount) {
+        this.moneyAmount = this.moneyAmount.add(moneyAmount);
+    }
+    
     @Override
-    public Change purchaseItem(BigDecimal amount, int itemId) throws InsufficientFundsException, 
+    public Change buyItem(BigDecimal moneyAmount, int itemId) throws InsufficientFundsException, 
             VendingMachinePersistenceException {
         
         Item item = vendingMachineDao.getItem(itemId).get();
         
-        if (item.getCost().compareTo(amount) > 0) {
+        setMoneyAmountToZero();
+        
+        if (item.getCost().compareTo(moneyAmount) > 0) {
             throw new InsufficientFundsException("There are not enough funds to purchase this item");
         } else {
             vendingMachineDao.reduceItemInventory(itemId);
             return new Change(item.getCost());
         }
     } 
+    
+    private void setMoneyAmountToZero() {
+        this.moneyAmount = new BigDecimal("0");
+        this.moneyAmount.setScale(2, RoundingMode.HALF_UP);
+    }
 }
