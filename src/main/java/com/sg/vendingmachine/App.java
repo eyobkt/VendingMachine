2e58df4104626da134ed651acf3cd24734462c2e
hiapellet <hi@apellet.ca>
2021-09-15 12:07:27 -0300

    entry point in program

--- src/main/java/com/sg/vendingmachine/App.java
+++ src/main/java/com/sg/vendingmachine/App.java
@@ -1,9 +1,23 @@
 package com.sg.vendingmachine;
 
-/**
- *
- * @author Eyob
- */
+import com.sg.vendingmachine.controller.VendingMachineController;
+import com.sg.vendingmachine.dao.VendingMachineDao;
+import com.sg.vendingmachine.dao.VendingMachineDaoFileImpl;
+import com.sg.vendingmachine.service.VendingMachineService;
+import com.sg.vendingmachine.service.VendingMachineServiceImpl;
+import com.sg.vendingmachine.ui.UserIO;
+import com.sg.vendingmachine.ui.UserIOConsoleImpl;
+import com.sg.vendingmachine.ui.VendingMachineView;
+
 public class App {
-    
+    public static void main(String[] args) {
+        UserIO io = new UserIOConsoleImpl();
+        VendingMachineView view = new VendingMachineView(io);
+
+        VendingMachineDao dao = new VendingMachineDaoFileImpl();
+        VendingMachineService service = new VendingMachineServiceImpl(dao);
+        
+        VendingMachineController controller = new VendingMachineController(service, view);
+        controller.run();
+    }
 }
