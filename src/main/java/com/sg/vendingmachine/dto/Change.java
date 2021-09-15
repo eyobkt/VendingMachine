0d0f39d7b6e2535049f5cff2dfb38632adf44c2b
hiapellet <hi@apellet.ca>
2021-09-14 23:47:01 -0300

    Implemented Change Class

--- src/main/java/com/sg/vendingmachine/dto/Change.java
+++ src/main/java/com/sg/vendingmachine/dto/Change.java
@@ -1,18 +1,38 @@
 package com.sg.vendingmachine.dto;
 
-/**
- *
- * @author Eyob
- */
+import java.math.BigDecimal;
+
 public class Change {
-    int pennies;
-    int nickels;
-    int dimes;
-    int quarters;
-    int dollars;
+    private static final int PENNIES_IN_ONE_DOLLAR = 100;
+    private static final int PENNIES_IN_ONE_QUARTER = 25;
+    private static final int PENNIES_IN_ONE_DIME = 10;
+    private static final int PENNIES_IN_ONE_NICKEL = 5;
+    
+    private int pennies;
+    private int nickels;
+    private int dimes;
+    private int quarters;
+    private int dollars;
     
     public Change(int pennies) {
+        this.dollars = pennies / PENNIES_IN_ONE_DOLLAR;
+        pennies %= PENNIES_IN_ONE_DOLLAR;
         
+        this.quarters = pennies / PENNIES_IN_ONE_QUARTER;
+        pennies %= PENNIES_IN_ONE_QUARTER;
+        
+        this.dimes = pennies / PENNIES_IN_ONE_DIME;
+        pennies %= PENNIES_IN_ONE_DIME;
+        
+        this.nickels = pennies / PENNIES_IN_ONE_NICKEL;
+        pennies %= PENNIES_IN_ONE_NICKEL;
+        
+        this.pennies = pennies;
+    }
+    
+    public Change(BigDecimal amountInDollars) {
+        //we might have to throw an exception if scale is greater than 2
+        this(amountInDollars.movePointRight(2).intValueExact());
     }
 
     public int getPennies() {
@@ -34,4 +54,9 @@ public class Change {
     public int getDollars() {
         return dollars;
     }
+
+    @Override
+    public String toString() {
+        return "Change{" + "pennies=" + pennies + ", nickels=" + nickels + ", dimes=" + dimes + ", quarters=" + quarters + ", dollars=" + dollars + '}';
+    }  
 }
